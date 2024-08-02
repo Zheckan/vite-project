fd5faa586741a4caba4e261708e6253bae35ca40
Yevhenii Zhaivoronok <Zheckan1120@gmail.com>
2024-08-02 15:05:56 +0200

    Temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
+import classes from './App.module.css';
 import Body from "./Body";
 import Header from "./Header";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Body />
-    </>
+      <div className={classes.wrapper}>
+        {Array(30).fill(null).map((_, index) => (
+          <button className={classes.testButton}>Test {index + 1}</button>
+        ))}
+      </div>
   );
 }
 
